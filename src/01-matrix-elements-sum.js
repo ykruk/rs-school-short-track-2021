80898e85dc9a1e4cddd300095319702776002b72
ykruk <yuliyakruk@tut.by>
2021-04-07 12:33:12 +0300

    refactor: shorten 01-matrix-elements-sum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -15,14 +15,15 @@
  * The result should be 9
  */
 function getMatrixElementsSum(matrix) {
-  const sumFirstRow = matrix[0].reduce((acc, el) => acc + el);
-  let sumOtherRows = 0;
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[1].length; j++) {
-      if (matrix[i - 1][j] !== 0) sumOtherRows += matrix[i][j];
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!matrix[i - 1] || matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
+      }
     }
   }
-  return sumFirstRow + sumOtherRows;
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
