ad52d46f7937984b7eda0cffb820a4086717e3c6
ykruk <yuliyakruk@tut.by>
2021-04-08 22:32:15 +0300

    feat: add 11-encode-line solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  return str.split('').concat('').reduce((acc, el) => {
+    if (acc.letter === el) {
+      acc.count += 1;
+    } else if (acc.letter === '') {
+      acc.letter = el;
+      acc.count = 1;
+    } else {
+      acc.output += acc.count > 1 ? acc.count + acc.letter : acc.letter;
+      acc.letter = el;
+      acc.count = 1;
+    }
+    return acc;
+  }, { letter: '', count: 0, output: '' }).output;
 }
 
 module.exports = encodeLine;
